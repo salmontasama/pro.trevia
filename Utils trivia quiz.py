961ce3784b6fd743cbc41e0f8f829bdd8c265cd6
djambar123 <djambar1994@gmail.com>
2022-04-03 19:18:30 +0300

    lsat

--- Utils trivia quiz.py
+++ Utils trivia quiz.py
@@ -11,14 +11,14 @@ answer_1 = "d"
 
 for i in range(chances):
     answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
     if answer == answer_1:
         print("Correct!!, Good job\n")
         score+=10
         print("your score is: ",score)
         break
-    while answer not in choice:
-        print("Invalid input please answer using the alphabet letters a - d !")
-        answer = input("try again :")
     else:
         print("Incorrect!\n")
         print("The correct answer is:",answer_1,"\n")
@@ -30,14 +30,14 @@ answer_2 = "b"
 
 for i in range(chances):
     answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
     if answer == answer_2:
         print("Correct!!, very nice\n")
         score+=10
         print("your score is: ",score)
         break
-    while answer not in choice:
-        print("Invalid input please answer using the alphabet letters a - d !")
-        answer = input("try again :")
     else:
         print("Incorrect!\n")
         print("The correct answer is:",answer_2,"\n")
@@ -49,14 +49,14 @@ answer_3 = "c"
 
 for i in range(chances):
     answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
     if answer == answer_3:
         print("Correct!!, MY Nigga\n")
         score+=10
         print("your score is: ",score)
         break
-    while answer not in choice:
-        print("Invalid input please answer using the alphabet letters a - d !")
-        answer = input("try again :")
     else:
         print("Incorrect!\n")
         print("The correct answer is:",answer_3,"\n")
@@ -64,19 +64,21 @@ for i in range(chances):
 print("---------------------------------------")
 print("Second topic is 'FOOTBALL'")
 
+print("---------------------------------------")
+
 question_4 = print("4) Which is not an English Football Club?\n","(a) Manchester United \n (b) PSG \n (c) Chelsea \n (d) Liverpool")
 answer_4 = "b"
 
 for i in range(chances):
     answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
     if answer == answer_4:
         print("Correct!!, MY Man\n")
         score+=10
         print("your score is: ",score)
         break
-    while answer not in choice:
-        print("Invalid input please answer using the alphabet letters a - d !")
-        answer = input("try again :")
     else:
         print("Incorrect!\n")
         print("The correct answer is:",answer_4,"\n")
@@ -89,14 +91,14 @@ answer_5 = "b"
 
 for i in range(chances):
     answer = input("Enter your answer :")
-    if answer == answer_5:
-        print("Correct!!, MY Man\n")
-        score+=10
-        print("your score is: ",score)
-        break
     while answer not in choice_2:
         print("Invalid input please answer using the alphabet letters a or b !")
         answer = input("try again :")
+    if answer == answer_5:
+        print("Correct!!, Awesome \n")
+        score+=10
+        print("your score is: ",score)
+        break
     else:
         print("Incorrect!\n")
         print("The correct answer is:",answer_5,"\n")
@@ -109,14 +111,111 @@ answer_6 = "d"
 
 for i in range(chances):
     answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
     if answer == answer_6:
         print("Correct!!, On Fire\n")
         score+=10
         print("your score is: ",score)
         break
-    while answer not in choice:
-        print("Invalid input please answer using the alphabet letters a - d !")
-        answer = input("try again :")
+
     else:
         print("Incorrect!\n")
         print("The correct answer is:",answer_6,"\n")
+
+print("---------------------------------------")
+print("third topic is 'General Knowledge'")
+
+print("---------------------------------------")
+
+question_7 = print("7) Which animal is known as the 'Ship of the Desert?""\n","(a) Lion \n (b) Whale\n (c) Camel \n (d) Elephant")
+answer_7 = "c"
+
+for i in range(chances):
+    answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
+    if answer == answer_7:
+        print("Correct!!, On Fire\n")
+        score+=10
+        print("your score is: ",score)
+        break
+
+    else:
+        print("Incorrect!\n")
+        print("The correct answer is:",answer_7,"\n")
+
+
+print("---------------------------------------")
+
+question_8 = print("8) Who painted the Mona Lisa?""\n","(a) Leonardo da Vinci \n (b) Rembrandt\n (c) Van Gogh \n (d) Claude Monet")
+answer_8 = "a"
+
+for i in range(chances):
+    answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
+    if answer == answer_8:
+        print("Correct!!, King\n")
+        score+=10
+        print("your score is: ",score)
+        break
+
+    else:
+        print("Incorrect!\n")
+        print("The correct answer is:",answer_8,"\n")
+
+
+print("---------------------------------------")
+
+question_9 = print("9) Who is the highest spiritual leader of Tibet?""\n","(a) Dalai Lama \n (b) Damba Mali\n (c) prime minister \n (d) Mister Bean")
+answer_9 = "a"
+
+for i in range(chances):
+    answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
+    if answer == answer_9:
+        print("Correct!!, King\n")
+        score+=10
+        print("your score is: ",score)
+        break
+
+    else:
+        print("Incorrect!\n")
+        print("The correct answer is:",answer_9,"\n")
+
+print("---------------------------------------")
+print("** BONUS POINTS **\n")
+print("Last question for double points")
+print("-------------------------------------")
+
+question_10 = print("10) Where did Takela Mekonen(Tech CEO) served in the IDF?""\n","(a) 8200 \n (b) GOLANI \n (c) Drone Unit \n (d) All of the above ")
+answer_10 = "c"
+
+for i in range(chances):
+    answer = input("Enter your answer :")
+    while answer not in choice:
+        print("Invalid input please answer using the alphabet letters a - d !")
+        answer = input("try again :")
+    if answer == answer_10:
+        print("Correct!!, ALUFIM\n")
+        score+=20
+        print("your score is: ",score)
+        break
+    else:
+        print("Incorrect!\n")
+        print("The correct answer is:",answer_10,"\n")
+if score < 70:
+    print("keep studying")
+elif 70 < score < 90 :
+    print("close to perfection")
+elif 90 < score < 100 :
+    print("You are a genius")
+elif score > 100:
+    print("You're too smart for this test")
+print("Game Over")
