8257f6c0c65451c09b61e549e3d53d12555a2d16
salmontasama <salmontasama1@gmail.com>
2022-04-04 10:55:27 +0300

    first fun

---
+++ utils.py
@@ -0,0 +1,41 @@
+def newGame():
+
+
+
+def chekAnser():
+    pass
+
+
+def displayScore():
+    pass
+
+
+def playAgain():
+    pass
+
+
+questions = {
+    'How many Infinity Stones are there?:': 'd',
+    'Who was able to pick up Thor’s hammer in Endgame?': 'b',
+    'Black Panther is set in which fictional country?': 'c',
+    'Which is not an English Football Club?': 'b',
+    'In 4-3-3 system, there are 3 Centre Backs?': 'b',
+    'Which team is not in the Uefa Champions League Semi Finals this year?': 'd',
+    'Which animal is known as the Ship of the Desert?': 'c',
+    'Who painted the Mona Lisa?': 'a',
+    'Who is the highest spiritual leader of Tibet?': 'a',
+    'Where did Takela Mekonen(Tech CEO) served in the IDF?': 'c'
+
+}
+options = [['(a) 4 \n (b) 2 \n (c) 8 \n (d) 6'],
+           ['(a) Black Panther \n (b) Captain America \n (c) Iron Man \n (d) Spiderman'],
+           ['(a) Ethiopia \n (b) London \n (c) Wakanda \n (d) Zimbabwe'],
+           ['(a) Manchester United \n (b) PSG \n (c) Chelsea \n (d) Liverpool'],
+           ['(a) True \n (b) False '],
+           ['(a) Real Madrid \n (b) Chelsea\n (c) Manchester City \n (d) Fc Barcelona'],
+           ['(a) Lion \n (b) Whale\n (c) Camel \n (d) Elephant'],
+           ['(a) Leonardo da Vinci \n (b) Rembrandt\n (c) Van Gogh \n (d) Claude Monet'],
+           ['(a) Dalai Lama \n (b) Damba Mali\n (c) prime minister \n (d) Mister Bean'],
+           ['(a) 8200 \n (b) GOLANI \n (c) Drone Unit \n (d) All of the above']]
+
+
